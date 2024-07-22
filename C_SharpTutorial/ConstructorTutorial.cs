b44db726873fb946c6ba4972b3f9c44b9213cb29
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2024-07-22 07:57:46 +0530

    create properties for accessing instance variable

--- C_SharpTutorial/ConstructorTutorial.cs
+++ C_SharpTutorial/ConstructorTutorial.cs
@@ -10,29 +10,54 @@ namespace C_SharpTutorial
     {
         //constructor are used to assign value to instance variable
         //constructor name should be same as classname
-        public int empId;
-        public string empName;
-        public string empRole;
-        public static string companyName ;
+        private int _empId;
+        private string _empName;
+        private string _empRole;
+        private static string _companyName ;
 
+        public int EmpId
+        {
+            set { _empId = value; }
+            get { return _empId; }
+        }
+
+        public string CompanyName
+        {
+            set { _companyName = value; }
+            get { return _companyName; }
+        }
+
+
+        public string EmpName
+        {
+            set { _empName = value; }
+            get { return _empName; }
+        }
+
+        // Property for EmpRole
+        public string EmpRole
+        {
+            set { _empRole = value; }
+            get { return _empRole; }
+        }
 
 
         public Employee(int empId, string empName, string empRole)
         {
-            this.empId = empId;
-            this.empName = empName;
-            this.empRole = empRole;
+            this._empId = empId;
+            this._empName = empName;
+            this._empRole = empRole;
         }
 
         static Employee()
         {
-            companyName = "Mismo Software";
+            _companyName = "Mismo Software";
         } 
         public void printEmployeeDeatils()
         {
-            Console.WriteLine("Employee ID-:"+empId);
-            Console.WriteLine("Employee Name-:" + empName);
-            Console.WriteLine("Employee Role-:" + empRole);
+            Console.WriteLine("Employee ID-:"+_empId);
+            Console.WriteLine("Employee Name-:" + _empName);
+            Console.WriteLine("Employee Role-:" + _empRole);
 
         }
     }
